7e6b2285bcccee8929815a871b13df57c65cc2c3
hanks <hanks6160@gmail.com>
2022-12-11 23:44:43 +0900

    MASTER

--- src/main/java/kr/infonation/dto/board/BoardDto.java
+++ src/main/java/kr/infonation/dto/board/BoardDto.java
@@ -1,11 +1,7 @@
 package kr.infonation.dto.board;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
+
 import com.querydsl.core.annotations.QueryProjection;
-import kr.infonation.domain.board.Board;
-import kr.infonation.domain.member.Member;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
@@ -16,15 +12,6 @@ public class BoardDto {
     private String name;
     private String login_id;
     private int viewCnt;
-
-    public BoardDto(Board board) {
-        this.board_id = board.getBoard_id();
-        this.title = board.getTitle();
-        this.content = board.getContent();
-        this.name = board.getUser().getName();
-        this.login_id = board.getUser().getLogin_id();
-        this.viewCnt = board.getViewCnt();
-    }
 
     @QueryProjection
     public BoardDto(Long board_id, String title, String content, String name, String login_id, int viewCnt) {
